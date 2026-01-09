2f0432203ae93a90c1143cc525c3f355f528b6e3
Márcio Rosa <marcio.pero@hotmail.com>
2026-01-09 15:29:15 -0300

    Organiza código criando classe Cadastro

---
+++ src/Cadastro.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Cadastro {
+
+    private ArrayList<Pessoa> pessoas = new ArrayList<>();
+
+    public void cadastrar(Pessoa pessoa) {
+        pessoas.add(pessoa);
+    }
+
+    public void listar() {
+        if (pessoas.isEmpty()) {
+            System.out.println("Nenhuma pessoa cadastrada.");
+        } else {
+            for (Pessoa p : pessoas) {
+                System.out.println("Nome: " + p.getNome() +
+                                   " | Idade: " + p.getIdade());
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Projeto Java com Git funcionando!");
+
+        Scanner sc = new Scanner(System.in);
+        Cadastro cadastro = new Cadastro();
+        int opcao;
+
+        do {
+            System.out.println("\n=== MENU ===");
+            System.out.println("1 - Cadastrar pessoa");
+            System.out.println("2 - Listar pessoas");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha: ");
+
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            if (opcao == 1) {
+                System.out.print("Nome: ");
+                String nome = sc.nextLine();
+
+                System.out.print("Idade: ");
+                int idade = sc.nextInt();
+                sc.nextLine();
+
+                cadastro.cadastrar(new Pessoa(nome, idade));
+                System.out.println("Pessoa cadastrada com sucesso!");
+
+            } else if (opcao == 2) {
+                cadastro.listar();
+            }
+
+        } while (opcao != 0);
+
+        sc.close();
+        System.out.println("Sistema finalizado.");
     }
 }
